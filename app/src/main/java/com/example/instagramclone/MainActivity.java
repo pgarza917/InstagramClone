a0bb530eb06ed36401aefefd9661a00d1b2fa7cd
Pablo Garza <pgarza917@fb.com>
2020-07-07 16:35:37 -0400

    Added post object and query functionality

--- app/src/main/java/com/example/instagramclone/MainActivity.java
+++ app/src/main/java/com/example/instagramclone/MainActivity.java
@@ -3,12 +3,54 @@ package com.example.instagramclone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    private EditText mEditTextDescription;
+    private Button mButtonCaptureImage;
+    private ImageView mImageViewPost;
+    private Button mButtonSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEditTextDescription = findViewById(R.id.editTextDescription);
+        mButtonCaptureImage = findViewById(R.id.buttonCaptureImage);
+        mImageViewPost = findViewById(R.id.imageViewPost);
+        mButtonSubmit = findViewById(R.id.buttonSubmit);
+
+        // Specify which class to query from Parse DB
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+
+        // Using findInBackground to pull all Posts from DB
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                // The ParseException will not be null if error with populating List with Post objects
+                // went wrong with query
+                if(e != null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return;
+                }
+                // The ParseException will be null if List was populated successfully from query
+                for(Post post : posts) {
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser().getUsername());
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/instagramclone/ParseApplication.java
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -3,11 +3,15 @@ package com.example.instagramclone;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Registering our Parse model
+        ParseObject.registerSubclass(Post.class);
 
         // set applicationId, and server server based on the values in the Heroku settings.
         // clientKey is not needed unless explicitly configured
